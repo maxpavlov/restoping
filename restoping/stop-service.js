92aa58f1202d813c321a92641f2d232fc10aee00
Max Pavlov <maxpavlov@Moment.local>
2015-08-16 14:34:46 +0300

    More SOA infrastructure refactoring

--- restoping/stop-service.js
+++ restoping/stop-service.js
@@ -1,19 +1,17 @@
 var Service = require('node-windows').Service;
 
-var scriptPath = process.argv[2]
-var serviceName = process.argv[3]
+var serviceName = process.argv[2]
 
 // Create a new service object
 var svc = new Service({
   name:serviceName,
   description: 'This is a node.js restoping site instance.',
-  script: scriptPath
 });
 
 // Listen for the "install" event, which indicates the
 // process is available as a service.
-svc.on('install',function(){
-  svc.start();
+svc.on('stop',function(){
+  console.log('Service stopped.');
 });
 
-svc.install();
+svc.stop();
--- restoping/uninstall-service.js
+++ restoping/uninstall-service.js
@@ -1,10 +1,13 @@
 var Service = require('node-windows').Service;
 
+var scriptPath = process.argv[2]
+var serviceName = process.argv[3]
+
 // Create a new service object
 var svc = new Service({
-  name:'Restoping',
+  name:serviceName,
   description: 'This is a node.js restoping site instance.',
-  script: '.\\bin\\www'
+  script: scriptPath
 });
 
 // Listen for the "uninstall" event so we know when it's done.
@@ -13,9 +16,4 @@ svc.on('uninstall',function(){
   console.log('The service exists: ',svc.exists);
 });
 
-svc.on('stop',function(){
-	// Uninstall the service.
-	svc.uninstall();
-});
-
-svc.stop();
+svc.uninstall();
